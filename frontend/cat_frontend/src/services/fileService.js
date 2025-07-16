291ec290c2bcaa91f31e39e387bdd166fd2b8c67
Aldina28 <aldinarr@gmail.com>
2025-07-16 17:21:41 +0530

    Sample

--- frontend/cat_frontend/src/services/fileService.js
+++ frontend/cat_frontend/src/services/fileService.js
@@ -4,7 +4,7 @@ export const uploadFile = (file, onUploadProgress) => {
   const formData = new FormData()
   formData.append('file', file)
 
-  return axios.post('/your-upload-endpoint', formData, {
+  return axios.post('http://', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     },
